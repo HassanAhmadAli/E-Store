6e0d264fc322a9d6d28e0337d7a32c6ce9a2fd8b
nourkaskas <nour9kaskas2001@gmail.com>
2025-07-07 16:17:40 +0300

    Add login and signup and home page

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,3 +1,75 @@
-export const LoginPage = function () {
-  return <h1>login page , not implemented</h1>;
-};
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useUserStore from "../../../store/userStore";
+import { toast } from "sonner";
+
+export function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const login = useUserStore((state) => state.login);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const foundUser = users.find(
+      (u) => u.email === email && u.password === password,
+    );
+
+    if (foundUser) {
+      login(foundUser);
+      toast.success("✅ Logged in successfully");
+      navigate("/");
+    } else {
+      toast.error("❌ Invalid credentials");
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-md rounded-lg bg-green-50 p-8 shadow-lg">
+        <h2 className="mb-6 text-center font-serif text-2xl font-semibold text-green-600">
+          Login
+        </h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full rounded-2xl border border-green-200 px-4 py-2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full rounded-2xl border border-green-200 px-4 py-2"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button
+            type="submit"
+            className="w-full rounded-2xl bg-green-400 py-2 font-serif text-white transition hover:bg-green-700"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="mt-4 text-center font-serif text-sm">
+          Do not have an account?{" "}
+          <button
+            onClick={() => navigate("/signup")}
+            className="text-green-600 hover:underline"
+          >
+            Sign Up
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/auth/signup/index.jsx
+++ src/pages/auth/signup/index.jsx
@@ -1,3 +1,74 @@
-export const SignupPage = function () {
-  return <h1>signup page , not implemented</h1>;
-};
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+export function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const existing = users.find((u) => u.email === email);
+
+    if (existing) {
+      toast.error("⚠️ User already exists!");
+      return;
+    }
+
+    const newUser = { name, email, password };
+    const updatedUsers = [...users, newUser];
+    localStorage.setItem("users", JSON.stringify(updatedUsers));
+
+    toast.success("🎉 Account created successfully, please login");
+    navigate("/login");
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
+      <div className="w-full max-w-md rounded-lg bg-green-50 p-8 shadow-lg">
+        <h2 className="mb-6 text-center font-serif text-2xl font-semibold text-green-600">
+          Sign Up
+        </h2>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full rounded-2xl border border-green-200 px-4 py-2"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full rounded-2xl border border-green-200 px-4 py-2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full rounded-2xl border border-green-200 px-4 py-2"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button
+            type="submit"
+            className="w-full rounded bg-green-400 py-2 text-white transition hover:bg-green-700"
+          >
+            Create Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
