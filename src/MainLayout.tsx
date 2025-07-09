2d4513eeba4b010cc20354ce1a1bb01abc071dcd
Hassan <hassan.ali.36900@outlook.com>
2025-07-09 19:17:54 +0300

    added basic navbar

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router";
+import { NavBar } from "@/components/navbar";
 export const MainLayout = function () {
   return (
     <div className="grid max-w-[100vw] gap-4 p-4">
       <div className="flex h-full w-full flex-col">
+        <nav>
+          <NavBar />
+        </nav>
         <main className="flex-1 p-4">
           <Outlet />
         </main>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { NavigationMenu } from "./ui/navigation-menu";
+export const NavBar = function () {
+  return <NavigationMenu></NavigationMenu>;
+};
