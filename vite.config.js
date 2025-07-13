5a3468475ce11c48ca2231ab24e422467ef8becb
Leendk <leen.dk12@gmail.com>
2025-07-13 21:09:46 +0300

    add react-query

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-        host: '0.0.0.0',
-        port: 5173,
-        allowedHosts: ['localhost', '.replit.dev', '.replit.co']
-      }
+  
   })
